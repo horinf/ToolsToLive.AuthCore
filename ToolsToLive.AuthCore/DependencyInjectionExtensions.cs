7fe634043319e4eb425b6cafa64dd14c9098153b
Троицкий Максим Алексеевич <horinf@mail.ru>
2020-02-01 17:53:51 +0300

    initial

--- ToolsToLive.AuthCore/DependencyInjectionExtensions.cs
+++ ToolsToLive.AuthCore/DependencyInjectionExtensions.cs
@@ -8,13 +8,23 @@ namespace ToolsToLive.AuthCore
 {
     public static class DependencyInjectionExtensions
     {
-        public static IServiceCollection AddAuthCore<TUser>(this IServiceCollection services, IConfigurationSection configurationSection) where TUser : IUser
+        /// <summary>
+        /// Sets up dependency for using auth core, including storage, auth options etc.
+        /// (storage is setting up as scoped service).
+        /// </summary>
+        /// <typeparam name="TUser">Type of the user in application.</typeparam>
+        /// <typeparam name="TUserStorage">Type of the storrage to set up IUserStorage dependency.</typeparam>
+        /// <param name="services">Service collection.</param>
+        /// <param name="configurationSection">Configuration section that should match to <see cref="AuthOptions"/> class.</param>
+        /// <returns></returns>
+        public static IServiceCollection AddAuthCore<TUser, TUserStorage>(this IServiceCollection services, IConfigurationSection configurationSection) where TUser : IUser where TUserStorage : class, IUserStorage<TUser>
         {
             services.Configure<AuthOptions>(configurationSection);
 
-            services.AddSingleton<IIdentityService, IdentityService>();
             services.AddSingleton<IPasswordHasher, PasswordHasher>();
-            services.AddSingleton<IAuthCoreService<TUser>, AuthCoreService<TUser>>();
+            services.AddScoped<IIdentityService, IdentityService>();
+            services.AddScoped<IAuthCoreService<TUser>, AuthCoreService<TUser>>();
+            services.AddScoped<IUserStorage<TUser>, TUserStorage>();
 
             return services;
         }
--- ToolsToLive.AuthCore/Model/AuthOptions.cs
+++ ToolsToLive.AuthCore/Model/AuthOptions.cs
@@ -4,10 +4,31 @@ namespace ToolsToLive.AuthCore.Model
 {
     public class AuthOptions
     {
+        /// <summary>
+        /// Someone who creates token (identity server).
+        /// Any valid string.
+        /// </summary>
         public string Issuer { get; set; }
+
+        /// <summary>
+        /// Someone for whome token should be created (consumer).
+        /// Any valid string.
+        /// </summary>
         public string Audience { get; set; }
+
+        /// <summary>
+        /// Strong key to sign token. (The longer string is, the stronger key -- 256 chars should be Ok).
+        /// </summary>
         public string Key { get; set; }
+
+        /// <summary>
+        /// Token lifetime.
+        /// </summary>
         public TimeSpan TokenLifetime { get; set; }
+
+        /// <summary>
+        /// Refresh token lifetime.
+        /// </summary>
         public TimeSpan RefreshTokenLifeTime { get; set; }
     }
 }
