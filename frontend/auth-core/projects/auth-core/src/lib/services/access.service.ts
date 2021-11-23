928c8892720913a262a711dda70fdc1693392983
Troitsky Maksim <horinf@mail.ru>
2021-11-23 21:35:32 +0300

    Access Service is in role

--- frontend/auth-core/projects/auth-core/src/lib/services/access.service.ts
+++ frontend/auth-core/projects/auth-core/src/lib/services/access.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import { AuthData } from '../model/AuthData';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class AccessService {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
-  isInOneOfRoles(authData: AuthData | null, ...role: string[]): boolean {
-    if (!role || !role.length) {
+  async isInOneOfRolesAsync(...role: string[]): Promise<boolean> {
+    const authData = await this.authService.getAuthDataAsync();
+    return this.isInOneOfRoles(authData, ...role);
+  }
+
+  isInOneOfRoles(authData: AuthData | null, ...roles: string[]): boolean {
+    if (!roles || !roles.length) {
       return false;
     }
-    const res = role.findIndex(x => this.isInRole(authData, x)) > -1;
+    const res = roles.findIndex(x => this.isInRole(authData, x)) > -1;
     return res;
+  }
+
+  async isInRoleAsync(role: string): Promise<boolean> {
+    const authData = await this.authService.getAuthDataAsync();
+    return this.isInRole(authData, role);
   }
 
   isInRole(authData: AuthData | null, role: string): boolean {
